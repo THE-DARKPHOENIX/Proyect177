d096fdd4e1c45d63b4c2750ea4b2b74d8fdfc440
THE-DARKPHOENIX <98428680+THE-DARKPHOENIX@users.noreply.github.com>
2024-03-08 17:45:21 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let words = [
+    {
+    "inputs": 5,
+    "category": "Deportes",
+    "word": "Ajedrez"
+    },
+    {
+    "inputs": 6,
+    "category": "Nombre del país europeo",
+    "word": "Francia"
+    },
+    ]
